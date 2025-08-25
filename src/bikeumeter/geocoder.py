62acb58e92a53d2de9c7beb1f7a6e17ed7dfa5be
Bernardo Ferrari <ber.97ferrari@gmail.com>
2025-08-25 20:24:45 +0200

    fix: fixed unknown address, now it works

--- src/bikeumeter/geocoder.py
+++ src/bikeumeter/geocoder.py
@@ -1,6 +1,7 @@
 import os
 import googlemaps
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
@@ -8,26 +9,29 @@ G_API_KEY = os.environ.get("G_API_KEY")
 gmaps = googlemaps.Client(key=G_API_KEY)
 
 def add_location(activity):
-    # start
-    start_lat = activity.get('start_latitude')
-    start_lon = activity.get('start_longitude')
-    # end
-    end_lat = activity.get('end_latitude')
-    end_lon = activity.get('end_longitude')
+    # pause to avoid hitting API rate limits
+    time.sleep(4)
 
-    # Default
+    start_coords = activity.get('start_latlng')
+    end_coords = activity.get('end_latlng')
+
+    # Default values
     activity['start_location'] = "Unknown"
     activity['end_location'] = "Unknown"
 
     try:
         # Start location
-        if start_lat and start_lon:
+        if start_coords and len(start_coords) == 2:
+            start_lat, start_lon = start_coords
+            print(f'Geocoding start: {start_lat}, {start_lon}')
             start_result = gmaps.reverse_geocode((start_lat, start_lon))
             if start_result:
                 activity['start_location'] = start_result[0]['formatted_address']
-        
+
         # End location
-        if end_lat and end_lon:
+        if end_coords and len(end_coords) == 2:
+            end_lat, end_lon = end_coords
+            print(f'Geocoding end: {end_lat}, {end_lon}')
             end_result = gmaps.reverse_geocode((end_lat, end_lon))
             if end_result:
                 activity['end_location'] = end_result[0]['formatted_address']
