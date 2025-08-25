8107f79db852fe69f4b188add6c239a73a15618e
Bernardo Ferrari <ber.97ferrari@gmail.com>
2025-08-25 18:57:32 +0200

    chore: sheets connection works

--- src/bikeumeter/spreadsheets.py
+++ src/bikeumeter/spreadsheets.py
@@ -1,11 +1,16 @@
 import gspread, os
 from google.oauth2.service_account import Credentials
+from dotenv import load_dotenv
+
+load_dotenv()
 
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = Credentials.from_service_account_file("sheets_creds.json", scopes=scopes)
+creds = Credentials.from_service_account_file("../../sheets_creds.json", scopes=scopes)
 client = gspread.authorize(creds)
 
 sheet_id = os.environ.get("GOOGLE_SHEET_ID")
+sheet = client.open_by_key(sheet_id)
 
-
+values_list = sheet.sheet1.row_values(1)
+print(values_list)
 
