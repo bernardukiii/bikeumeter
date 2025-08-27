cd55f883a4f86497a4523d45a4b0243402183d02
Bernardo Ferrari <ber.97ferrari@gmail.com>
2025-08-27 08:39:02 +0200

    chore: limit get request to bring in only 4 newest activities

--- src/bikeumeter/strava_api.py
+++ src/bikeumeter/strava_api.py
@@ -11,10 +11,10 @@ commute_acts = []
 
 def get_activities():
     url = f"{STRAVA_API}"
-    headers = {
-        "Authorization": f"Bearer {STRAVA_ACCESS_TOKEN}"
-    }
-    response = requests.get(url, headers=headers)
+    headers = { "Authorization": f"Bearer {STRAVA_ACCESS_TOKEN}" }
+    params = { "per_page": 4 } # i'm only interested in the latest ones || 4 in case I do 2 commute activities per day, but could be 2
+
+    response = requests.get(url, headers=headers, params=params)
     response.raise_for_status() # if errors
     # raw activities = all of them
     unfiltered_acts = response.json()
@@ -23,4 +23,6 @@ def get_activities():
         act_name = act['name'].lower()
         if 'commute' in act_name:
             commute_acts.append(act)
+            commute_acts_length = commute_acts.len()
+            print('commutes: ', commute_acts_length)
     return commute_acts
