a42c50784c6d53e65e19c1aedeca29bc5fc3d0e2
Bernardo Ferrari <ber.97ferrari@gmail.com>
2025-08-29 14:53:52 +0200

    fix: return both tokens

--- src/bikeumeter/token_flow.py
+++ src/bikeumeter/token_flow.py
@@ -24,4 +24,4 @@ def refresh_access_token():
     print("✅ Tokens refreshed successfully!")
 
     # On GitHub Actions, you can’t write .env — return them instead
-    return new_access_token, new_refresh_token
+    return tokens["access_token"], tokens["refresh_token"]
