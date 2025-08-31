b518f843d4e7e6f15dc61cea902684c34cb2559c
Bernardo Ferrari <ber.97ferrari@gmail.com>
2025-08-31 10:26:59 +0200

    fix: corrected selectors

--- src/bikeumeter/fare_scraper.py
+++ src/bikeumeter/fare_scraper.py
@@ -6,49 +6,28 @@ def scrape_fare(page: Page):
     
     time.sleep(1)
 
+    # CLOSE COOKIE POPUP
+    cookie_popup_btn = page.get_by_role("button", name="Weigeren")
+    cookie_popup_btn.wait_for()
+    cookie_popup_btn.click()
+
+    time.sleep(0.5)
+
     # Expect a title "to contain" a substring.
     # going to have to do it for both english and dutch in case it opens either version
     title = page.title()
-
-    if re.search("Where do you want to go?", title):
-        print('Eng version')
+    print(page.accessibility.snapshot())
+    if title:
+        print('Page title ', title)
         # find input, click on it, and input start address
-        from_input = page.get_by_role("textbox", name="from")
-        from_input.wait_for()
-        from_input.click()
-
-        from_input.type('Ganzenhoef', delay=100)
-        time.sleep(0.1)
-
-        to_input = page.get_by_role("textbox", name="to")
-        to_input.wait_for()
-        to_input.click()
-        time.sleep(0.2)
-        to_input.type('Rokin 69', delay=100)
-        time.sleep(0.8)
-
-        # fill in timetable | always 9am
-        time_input = page.get_by_role("textbox", name="time")
-        time_input.wait_for()
-        time_input.click()
-        time.sleep(0.1)
-        time_input.type('09:00', delay=100)
-        time.sleep(1)
-
-        # click on the button and trigger the search
-        page.get_by_role("button", name="Plan your trip").click()
-
-    elif re.search("Waar wil je heen?", title):
-        print('NL version')
-        # find input, click on it, and input start address
-        from_input = page.get_by_role("textbox", name="from")
+        from_input = page.get_by_role("combobox", name="van")
         from_input.wait_for()
         from_input.click()
 
         from_input.type('Ganzenhoef', delay=100)
         time.sleep(0.3)
 
-        to_input = page.get_by_role("textbox", name="to")
+        to_input = page.get_by_role("combobox", name="naar")
         to_input.wait_for()
         to_input.click()
         time.sleep(0.2)
@@ -56,7 +35,7 @@ def scrape_fare(page: Page):
         time.sleep(0.8)
 
         # fill in timetable | always 9am
-        time_input = page.get_by_role("textbox", name="time")
+        time_input = page.get_by_role("combobox", name="tijd")
         time_input.wait_for()
         time_input.click()
         time.sleep(0.1)
